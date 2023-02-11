5be82e7a698f7548fbbeb19247ef2b65dc310f9c
Sara Jain <sarajain3012@gmail.com>
2023-02-11 20:41:40 +0100

    graphics

---
+++ graphics.py
@@ -0,0 +1,16 @@
+import turtle
+
+rod = turtle.Pen()
+rod.shape("turtle")
+rod.speed(10)
+rod.color("red")
+rod.width(3)
+rod.left(180)
+rod.forward(150)
+rod.left(90)
+rod.forward(120)
+rod.left(135)
+rod.circle(-100, 270)
+
+
+input()
