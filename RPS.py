ea510c97ecc75cff47da49d68b5580c77bfe30f9
Sara Jain <sarajain3012@gmail.com>
2023-02-11 19:31:35 +0100

    Update RPS

--- RPS.py
+++ RPS.py
@@ -1,29 +1,49 @@
 import random
 
-computer = random.randint(1,3)
+score = 0
 
-choice = int(input("type 1 for rock, 2 for paper or 3 for scissors :"))
+while True:
+    word = input("type rock, paper, scissors or quit: ")
 
-if computer == 1:
-    print("I picked rock ")
-elif computer == 2:
-    print("I picked paper ")
-elif computer == 3:
-    print("I picked scissors ")
+    if word == "rock":
+        choice = 1
+    elif word == "paper":
+        choice = 2
+    elif word == "scissors":
+        choice = 3
+    elif word == "quit":
+        print(f"you scored {score} points ")
+        break
+    else:
+        print("sorry that does not work you have to type rock, paper, scissors or quit ")
+        continue
 
+    computer = random.randint(1,3)
 
-if computer == choice:
-    print("it's a tie ")
-elif computer == 1 and choice == 2:
-    print("you win! ")
-elif computer == 2 and choice == 3:
-    print("you win! ")
-elif computer == 3 and choice == 1:
-    print("you win! ")
-else:
-    print("you lose ")
-
+    if computer == 1:
+        print("I picked rock ")
+    elif computer == 2:
+        print("I picked paper ")
+    elif computer == 3:
+        print("I picked scissors ")
     
+    
+    if computer == choice:
+        print("it's a tie ")
+    elif computer == 1 and choice == 2:
+        score = score + 1
+        print("you win! ")
+    elif computer == 2 and choice == 3:
+        score = score + 1
+        print("you win! ")
+    elif computer == 3 and choice == 1:
+        score = score + 1
+        print("you win! ")
+    else:
+        score = score - 1
+        print("you lose ")
+
+        
 
 
 
